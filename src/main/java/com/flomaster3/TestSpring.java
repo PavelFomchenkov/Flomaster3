2438230b940905b62f7e18178b64710274a8fd92
PavelFomchenkov <donotwritemail@gmail.com>
2023-05-27 20:01:42 +0300

    lesson 13 homework done

--- src/main/java/com/flomaster3/TestSpring.java
+++ src/main/java/com/flomaster3/TestSpring.java
@@ -6,7 +6,7 @@ public class TestSpring {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusicList(Genre.POP);
+        musicPlayer.playMusicList(Genre.ROCK);
         context.close();
     }
 }
